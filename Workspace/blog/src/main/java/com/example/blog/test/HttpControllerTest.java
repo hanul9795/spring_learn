d639c2b838a2d63a33f9a965a68186a535576c15
hanul9795 <hanul9795@gmail.com>
2024-04-29 11:20:59 +0900

    15-17강 Maven - pom.xml 라이브러리(의존성) 추가 매커니즘 Lombok Getter, Setter, Constructor 종류(AllArgs, No, RequiredArgs), Builder Xml,json,yaml 차이점 RestController, Controller차이점 파일 리턴 경로 설정시 주의점 오전 11시25분종료

--- Workspace/blog/src/main/java/com/example/blog/test/HttpControllerTest.java
+++ Workspace/blog/src/main/java/com/example/blog/test/HttpControllerTest.java
@@ -11,17 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest : ";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		//Member m = new Member(1,"ssar","1234","email");
+		Member m = Member.builder().username("ssar").password("1234").email("email").build();
+		System.out.println(TAG+"getter : " + m.getUsername() );
+		m.setUsername("hanul");
+		System.out.println(TAG+"setter : " +  m.getUsername());
+		return "lombok test 완료";
+	}
+	
+	
 	//http://localhost:8080/http/get(selete)
 	@GetMapping("/http/get")
 	//public String getTest(@RequestParam int id, @RequestParam String username) {
 	public String getTest(Member m) { // id=1&username=ssar&password=1234&email=ssar@nate.com를(쿼리 스트링) Member에 넣는 것을 Spring이 해줌 MessageConverter(스프링부트) - 들어온 데이터 자동 파싱
+		
 		return "get 요청 : " + m.getId() + ", "+ m.getUsername()+ ", "+ m.getPassword()+ ", "+ m.getEmail();
 	}
 	//http://localhost:8080/http/post(incert)
 	//인터넷 브라우저 요청은 무조건 get 요청밖에 되지 않는다
 	@PostMapping("/http/post")
 	 //raw = text/plain == String text, application/json == Member m
-	public String postTest(@RequestBody Member m) {//MessageConverter(스프링부트) - 들어온 데이터 자동 파싱
+	public String postTest(@RequestBody Member m) {//MessageConver+ter(스프링부트) - 들어온 데이터 자동 파싱
 		return "post 요청 : " + m.getId() + ", "+ m.getUsername()+ ", "+ m.getPassword()+ ", "+ m.getEmail();
 	}
 	//	(update)
--- Workspace/blog/src/main/java/com/example/blog/test/Member.java
+++ Workspace/blog/src/main/java/com/example/blog/test/Member.java
@@ -1,41 +1,30 @@
 package com.example.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+@Data // @Getter + @Setter
+//@AllArgsConstructor //모든 필드를 다 쓰는 생성자 생성
+@NoArgsConstructor //빈 생성자
+//@RequiredArgsConstructor //final 붙은 변수에 대한 생성자 생성 final 없으면 안 만듬
 public class Member {
-	private int id;
-	private String username;
-	private String password;
-	private String email;
+	private  int id;
+	private  String username;
+	private  String password;
+	private  String email;
 	
-	
-	
+	@Builder
 	public Member(int id, String username, String password, String email) {
 		this.id = id;
 		this.username = username;
 		this.password = password;
-		this.email = email;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
 		this.email = email;
 	}
 	
